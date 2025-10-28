3c856abd87b25e34189e8aa34fc4594e011a7ce7
Stephan Heuscher <x+git@yic.ch>
2025-10-28 23:41:35 +0100

    Version 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ch.heuscher.back_home_dot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
